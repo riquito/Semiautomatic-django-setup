06a1e480cda68acb0b7b4b5536cb7951159dbcf0
Riccardo Attilio Galli <riccardo@sideralis.org>
2013-03-17 17:45:49 +0100

    Update manage.py, now use the same of django 1.5

--- base_project/manage.py
+++ base_project/manage.py
@@ -1,24 +1,10 @@
 #!/usr/bin/env python
-
-import os,imp
-from os.path import join,dirname,abspath
-
-conf_target = os.environ.get('TARGET','develop')
-conf_path = abspath(join(dirname(__file__),'configs',conf_target))
-
-try:
-    fp,path,desc = imp.find_module('settings',[conf_path])
-    try: 
-      settings = imp.load_module('settings',fp,path,desc)
-    finally: 
-      fp.close()
-    
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory configs/%s. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % conf_target)
-    sys.exit(1)
-
+import os
+import sys
 
 if __name__ == "__main__":
-    from django.core.management import execute_manager
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.local")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
